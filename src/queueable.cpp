a085076b285d05e1ed81099b1a1dea4816bcfad8
tvboelens <tvboelens@gmail.com>
2025-03-17 21:54:09 +0100

    Use getter method, since we cannot access job id directly anymore

--- src/queueable.cpp
+++ src/queueable.cpp
@@ -6,7 +6,7 @@ Queueable::Queueable(/* args */)
 
 void Queueable::dispatch(const json &args){
     Job *job = new Job(args);
-    spdlog::info("Enqueued job id={}", job->id);
+    spdlog::info("Enqueued job id={}", job->get_id());
     //spdlog::info("args={}", job->args);
     job->save();
     delete job;
