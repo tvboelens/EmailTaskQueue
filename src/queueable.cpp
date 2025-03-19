d10df858e39cd4f39429aa973cdef5c2e7d9b07d
tvboelens <tvboelens@gmail.com>
2025-03-19 18:45:56 +0100

    Use job instance in dispatch method instead of pointer to job

--- src/queueable.cpp
+++ src/queueable.cpp
@@ -5,9 +5,8 @@ Queueable::Queueable(/* args */)
 }
 
 void Queueable::dispatch(const json &args){
-    Job *job = new Job(args);
-    spdlog::info("Enqueued job id={}", job->get_id());
+    Job job{args};
+    spdlog::info("Enqueued job id={}", job.get_id());
     //spdlog::info("args={}", job->args);
-    job->save();
-    delete job;
+    job.save();
 }
