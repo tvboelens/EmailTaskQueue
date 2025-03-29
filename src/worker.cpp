56978a8eecea15495beaed5235136090bd7b4477
tvboelens <tvboelens@gmail.com>
2025-03-29 17:38:40 +0100

    First version of retry logic with both fixed and exponential decay.

--- src/worker.cpp
+++ src/worker.cpp
@@ -36,7 +36,7 @@ void Worker::run()
     else{
         spdlog::info("Established database connection: worker {}", worker_id);
     }
-    int counter = 0;
+
     do
     {
         std::unique_ptr<Job> job = next_job(db);
@@ -44,16 +44,12 @@ void Worker::run()
         {
             spdlog::info("Worker {} executing job: {} with name = {}", worker_id, job->get_id(), job->get_name());
             execute_job(*job);
-            cleanup_job(*job);
-            counter += 1;
         }
         else
         {
             sleep(polling_interval);
-            counter += 1;
         }
     } while (!stopWorkers);
-    spdlog::info("Worker {} shutting down connection to database", worker_id);
     spdlog::info("Shutting down Worker {}", worker_id);
 }
 
@@ -66,25 +62,41 @@ std::unique_ptr<Job> Worker::next_job(sqlite3 *db){
         WHERE id = (
             SELECT id FROM jobs 
             WHERE reserved_by IS NULL 
-            AND state = 'waiting'
-            AND (next_execution_at IS NULL OR next_execution_at <= CURRENT_TIMESTAMP)
+            AND (state = 'waiting' OR state = 'failed')
+            AND (next_execution_at IS NULL OR next_execution_at <= ?)
             ORDER BY created_at ASC         -- Retrieve job which was created first
             LIMIT 1
         )
-        RETURNING id, args, name, state;
+        RETURNING id, args, name, queue, attempts;
     )";
 
     if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK)
     {
-        // Bind worker_id as a TEXT value
+        // Bind worker_id and current timestamp as a TEXT value
         sqlite3_bind_text(stmt, 1, worker_id.c_str(), -1, SQLITE_STATIC);
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+        std::tm *localTime = std::localtime(&now_c);
+
+        std::ostringstream oss;
+        oss << std::put_time(localTime, "%Y-%m-%d %H:%M:%S"); // SQL datetime format
+        std::string timestamp = oss.str();
+        sqlite3_bind_text(stmt, 2, timestamp.c_str(), -1, SQLITE_STATIC);
         if (sqlite3_step(stmt) == SQLITE_ROW)
         {
             std::string id = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
             std::string args_str = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
             json args = nlohmann::json::parse(args_str);
             std::string name = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2));
-            std::unique_ptr<Job> job{new Job{id, args, name}};
+            std::string queue = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3));
+            int attempts = sqlite3_column_int(stmt, 4);
+            std::unique_ptr<Job> job{new Job{
+                id,
+                args,
+                name,
+                queue,
+                attempts
+            }};
             spdlog::info("Worker {}. Fetched next job: {}", worker_id, job->get_id());
             sqlite3_finalize(stmt);
             return job;
@@ -173,8 +185,7 @@ void Worker::execute_job(Job &job)
 
 void Worker::cleanup_job(Job &job, bool succeeded)
 {
-    job.set_reserved_by(std::nullopt);  // Set job as not reserved by any worker
-    job.increase_attempts();
+    job.set_reserved_by(std::nullopt); 
     std::chrono::system_clock::time_point time = std::chrono::system_clock::now();
     job.set_latest_attempt_to_now();
     if (succeeded){
@@ -182,8 +193,68 @@ void Worker::cleanup_job(Job &job, bool succeeded)
     }
     else
     {
+        job.increase_attempts();
         job.set_state("failed");
+        set_retry_details(registry->getRetryStrategy(job.get_name()), job);
     }
     spdlog::info("Worker {}. Done cleaning up job {} with name = {}, saving...", worker_id, job.get_id(), job.get_name());
     job.save(db);
+}
+
+void Worker::set_retry_details(const RetryStrategy &strategy,
+                               Job &job)
+{
+    std::optional<double> interval;
+    if (strategy.interval.has_value())
+    {
+        interval.emplace(strategy.interval.value());
+    }
+    else
+    {
+        interval = std::nullopt;
+    }
+
+    std::string queue = strategy.queue.has_value() ? strategy.queue.value() : "default"; 
+    int max_retries = strategy.max_retries.has_value() ? strategy.max_retries.value() : -1;
+
+    int retry_count = job.get_attempts() - 1;
+    // Check if max retries exceeded
+    if (max_retries != -1 && retry_count +1 >= max_retries)
+    {
+        job.set_state("dead");
+        return;
+    }
+
+    if (strategy.block)
+    {
+        std::optional<double> decision{strategy.block(retry_count)};
+        if (!decision)
+        {
+            job.set_state("dead");
+            return;
+        }
+        interval = std::move(decision);
+    }
+
+    // Exponential backoff calculation
+    if (interval.has_value() && interval.value() == -1)
+    { // Use -1 as a placeholder for exponential
+        interval.emplace(30 + std::pow(retry_count, 5));
+    }
+
+    if(job.get_last_attempt().has_value())
+    {
+        job.set_next_attempt(job.get_last_attempt().value() +
+                std::chrono::duration_cast<std::chrono::system_clock::duration>(std::chrono::duration<double>(interval.value())));
+    }
+    else
+    {
+        job.set_next_attempt(std::chrono::system_clock::now() + 
+            std::chrono::duration_cast<std::chrono::system_clock::duration>(std::chrono::duration<double>(interval.value())));
+    }
+
+    if (!queue.empty())
+    {
+        job.set_queue(queue);
+    }
 }
